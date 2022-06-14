1cb55695eac3e33a5ffb60b90b337bf548b2ea9e
游承祐 <eric21364@gmail.com>
2022-06-14 20:04:06 +0800

    調整router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,10 @@ import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DetailComponent } from './detail/detail.component';
 const routes: Routes = [
-  // { path: "**", redirectTo: "home" },
   { path: "home", component: HomeComponent },
   { path: "favourite", component: FavouriteComponent },
   { path: "detail", component: DetailComponent },
+  { path: "**", redirectTo: "home" },
 
 ];
 
